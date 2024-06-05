9bdf2381b39df1a5151962da7b9554c6dbc8ecad
maopequena <127451571+maopequena@users.noreply.github.com>
2024-06-05 15:10:39 -0300

    finalização dia 01

---
+++ 01_Day_Introduction/helloworld_mpq.py
@@ -0,0 +1,24 @@
+# 30 Days of Code Challenge
+
+# Day 1
+
+# Numbers and mathematical operators
+
+print(3+5)     # addition
+print(10-7)    # subtraction
+print(6*4)     # multiplication
+print(8/3)     # division
+print(2**3)    # exponential
+print(5%2)     # modulus
+print(7//2)    # floor
+
+# Checking data types
+
+print(type(10))                              # Int
+print(type(3.14))                            # float
+print(type(1+3j))                            # complex
+print(type("Lizandra"))                      # string
+print(type([4, 23, 7, 1]))                   # list
+print(type({'cães':'Bueri, Frizzy, Koda'}))  # dictionary
+print(type({9.3, 6.7, 8}))                   # set
+print(type((9.3, 6.7, 8)))                   # tuple
