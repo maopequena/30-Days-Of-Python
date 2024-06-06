1602d331040ca8b3b95f16b15d0156f597f2123b
maopequena <127451571+maopequena@users.noreply.github.com>
2024-06-06 16:49:51 -0300

    End of day 2

---
+++ 02_Day_Variables_builtin_functions/variables_liz.py
@@ -0,0 +1,59 @@
+# Day 2 of the 30 Days of Python challenge!
+
+firstName = 'Lizandra'
+lastName = 'Silva'
+fullName = 'Lizandra Silva'
+country - 'Brasil'
+city = 'Piracaia'
+age = 44
+year = 2024
+isMarried = False
+isTrue = True # True if person has dogs, False if person doesn't have dogs
+isLightOn - False # True if the light is on, False if it is off
+name, surname, birthYear, hasCats = 'Lizandra', 'Silva', 1979, True
+num1 = 5
+num2 = 4
+sum_nums = (num1, num2)
+diff = num1-num2
+product = num1*num2
+division = num1/num2
+remainder = num2%num1
+exp = num1**num2
+floor = num1//num2
+_pi = 3.14
+radius = input('What is the radius of the circle?')
+area = _pi*(int(radius)**2)
+circumference = (2*_pi*radius)
+
+_name = input('Type your first name: ')
+_surname = input('Type your last name: ')
+_age = input('Type your age: ')
+_country = input('Type your country: ')
+
+type(fullName)
+
+type(year)
+
+type(hasCats)
+
+len(firstName)
+
+max(len(firstName), len(lastName))
+
+sum(sum_nums)
+
+print(diff)
+
+print(product)
+
+print(division)
+
+print(remainder)
+
+print(exp)
+
+print(floor)
+
+print(area)
+
+print(circumference)
