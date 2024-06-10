8ccffd4fd42c30016f0ff636b3b30d1e15667c7a
maopequena <127451571+maopequena@users.noreply.github.com>
2024-06-10 19:39:37 -0300

    end of day 5

---
+++ 04_Day_Strings/04.py
@@ -0,0 +1,14 @@
+coding = 'Coding'
+_for = 'for'
+_all = 'All'
+space = ' '
+
+conc = coding + space + _for + space + _all
+print(conc)
+
+company = conc
+
+print(company)
+
+print(company.upper())
+
